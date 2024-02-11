c0e1a2822f1023cde5f424db58ab146cbe5fc3da
PranavPol-01 <138373185+PranavPol-01@users.noreply.github.com>
2024-02-11 12:16:17 +0530

    update login and guideline

--- src/Pages/GuidelinesPage.jsx
+++ src/Pages/GuidelinesPage.jsx
@@ -18,7 +18,7 @@ function GuidelinesPage() {
       </h1>
       <p className="mb-8 leading-relaxed text-green-300">Dive into Essential Waste Management Manuals and Guides!</p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-green-500 border-0 rounded-full py-1 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+        <button className="inline-flex text-white bg-green-500 border-0 rounded-full py-1 px-6 focus:outline-none hover:bg-green-600  text-lg">Guide Me </button>
         {/* <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button> */}
       </div>
     </div>
@@ -102,27 +102,28 @@ function GuidelinesPage() {
           <h1 className="text-3xl font-medium title-font mb-4 text-green-400 tracking-widest">Manuals</h1>
           {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p> */}
         </div>
-        <div className="flex flex-wrap -m-4 pl-12">
-          <div className="p-4 lg:w-1/2 ">
-            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-lg shadow-green-400/60">
-            <img alt="team" className="flex-shrink-0  w-40 h-40 mx-5 object-cover object-center sm:mb-0 mb-20 rounded-full" src="https://dummyimage.com/200x200" />
+        <div className="flex flex-wrap ">
+          
+          <div className="p-4 lg:w-1/2  ">
+            <div className=" h-full  flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-green-400/60 shadow-lg">
+            <img alt="team" className="flex-shrink-0  p-5 w-40 h-40 mx-5 object-cover object-center sm:mb-0  rounded-full" src="https://dummyimage.com/200x200" />
               {/* <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={require('https://dummyimage.com/200x200')} /> */}
               <div className="flex-grow sm:pl-8">
-                <h2 className="title-font font-medium text-lg text-gray-900">Household Waste </h2>
+                <h2 className="title-font font-medium text-lg text-gray-900">Household Waste</h2>
                 {/* <h3 className="text-gray-500 mb-3">UI Developer</h3> */}
-                <p className="mb-4">Importance of waste management at home</p>
+                <p className="mb-4 m-4">Importance of waste management at home</p>
                 <span className="inline-flex">
-                  <a href="#" className="text-gray-500">
+                  <a href="#" className="text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="ml-2 text-gray-500">
+                  <a href="#" className="ml-2 text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="ml-2 text-gray-500">
+                  <a href="#" className="ml-2 text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                     </svg>
@@ -131,55 +132,29 @@ function GuidelinesPage() {
               </div>
             </div>
           </div>
+          {/*  */}
           {/* Repeat the above structure for other team members */}
-          <div className="p-4 lg:w-1/2 ">
-            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-lg shadow-green-400/60 ">
-            <img alt="team" className="flex-shrink-0  w-40 h-40 mx-5 my-5 object-cover object-center sm:mb-0 mb-4 rounded-full" src="https://dummyimage.com/200x200" />
+      
+          <div className="p-4 lg:w-1/2  ">
+            <div className=" h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-green-400/60 shadow-lg">
+            <img alt="team" className="flex-shrink-0 p-5 w-40 h-40 mx-5 object-cover object-center sm:mb-0  rounded-full" src="https://dummyimage.com/200x200" />
               {/* <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={require('https://dummyimage.com/200x200')} /> */}
               <div className="flex-grow sm:pl-8">
                 <h2 className="title-font font-medium text-lg text-gray-900">Hazardous Waste Handling</h2>
                 {/* <h3 className="text-gray-500 mb-3">UI Developer</h3> */}
-                <p className="mb-4">Focus on hazardous waste management</p>
+                <p className="mb-4 m-4">Focus on hazardous waste management</p>
                 <span className="inline-flex">
-                  <a href="#" className="text-gray-500">
+                  <a href="#" className="text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="ml-2 text-gray-500">
+                  <a href="#" className="ml-2 text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="ml-2 text-gray-500">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="p-4 lg:w-1/2 ">
-            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-lg shadow-green-400/60 ">
-            <img alt="team" className="flex-shrink-0  w-40 h-40 mx-5 object-cover object-center sm:mb-0 mb-4 rounded-full" src="https://dummyimage.com/200x200" />
-              {/* <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={require('https://dummyimage.com/200x200')} /> */}
-              <div className="flex-grow sm:pl-8">
-                <h2 className="title-font font-medium text-lg text-gray-900">Industrial Waste</h2>
-                {/* <h3 className="text-gray-500 mb-3">UI Developer</h3> */}
-                <p className="mb-4">Address waste management in industrial settings..</p>
-                <span className="inline-flex">
-                  <a href="#" className="text-gray-500">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a href="#" className="ml-2 text-gray-500">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a href="#" className="ml-2 text-gray-500">
+                  <a href="#" className="ml-2 text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                     </svg>
@@ -190,24 +165,53 @@ function GuidelinesPage() {
           </div>
           <div className="p-4 lg:w-1/2  ">
             <div className=" h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-green-400/60 shadow-lg">
-            <img alt="team" className="flex-shrink-0 w-40 h-40 mx-5 object-cover object-center sm:mb-0 my-6 rounded-full" src="https://dummyimage.com/200x200" />
+            <img alt="team" className="flex-shrink-0 p-5 w-40 h-40 mx-5 object-cover object-center sm:mb-0  rounded-full" src="https://dummyimage.com/200x200" />
+              {/* <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={require('https://dummyimage.com/200x200')} /> */}
+              <div className="flex-grow sm:pl-8">
+              <h2 className="title-font font-medium text-lg text-gray-900">Industrial Waste</h2>
+                {/* <h3 className="text-gray-500 mb-3">UI Developer</h3> */}
+                <p className="mb-4 mr-4">Address waste management in industrial settings..</p>
+                <span className="inline-flex">
+                  <a href="#" className="text-green-500">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                  <a href="#" className="ml-2 text-green-500">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                  <a href="#" className="ml-2 text-green-500">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+         
+          <div className="p-4 lg:w-1/2  ">
+            <div className=" h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left bg-white rounded-md shadow-green-400/60 shadow-lg">
+            <img alt="team" className="flex-shrink-0 p-5  w-40 h-40 mx-5 object-cover object-center sm:mb-0  rounded-full" src="https://dummyimage.com/200x200" />
               {/* <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={require('https://dummyimage.com/200x200')} /> */}
               <div className="flex-grow sm:pl-8">
                 <h2 className="title-font font-medium text-lg text-gray-900">Community Waste Awareness</h2>
                 {/* <h3 className="text-gray-500 mb-3">UI Developer</h3> */}
                 <p className="mb-4">Educate the community on waste management.</p>
                 <span className="inline-flex">
-                  <a href="#" className="text-gray-500">
+                  <a href="#" className="text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="ml-2 text-gray-500">
+                  <a href="#" className="ml-2 text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="ml-2 text-gray-500">
+                  <a href="#" className="ml-2 text-green-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                     </svg>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,32 +3,34 @@ import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div><div className="bg-white py-6 sm:py-8 lg:py-12">
-    <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Login</h2>
+    <>
+    <div className="bg-green-700 ">
+      <div className=" py-6 sm:py-8 lg:py-12 w-screen h-screen">
+    <div className="mx-auto bg-white max-w-xl py-10 px-4  rounded-lg md:px-8">
+      <h2 className="mb-4 text-center text-2xl font-bold text-green-600 md:mb-8 lg:text-3xl">Login</h2>
   
       <form className="mx-auto max-w-lg rounded-lg border">
         <div className="flex flex-col gap-4 p-4 md:p-8">
           <div>
-            <label for="email" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
-            <input name="email" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+            <label for="email" className="mb-2 inline-block text-sm text-green-600 sm:text-base">Email</label>
+            <input name="email" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-green-300 transition duration-100 focus:ring" />
           </div>
   
           <div>
-            <label for="password" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Password</label>
-            <input name="password" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+            <label for="password" className="mb-2 inline-block text-sm text-green-600 sm:text-base">Password</label>
+            <input name="password" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-green-300 transition duration-100 focus:ring" />
           </div>
   
-          <button className="block rounded-lg bg-gray-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">Log in</button>
+          <button className="block rounded-lg bg-green-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-green-300 transition duration-100 hover:bg-green-700 focus-visible:ring active:bg-green-600 md:text-base">Log in</button>
   
           <div className="relative flex items-center justify-center">
-            <span className="absolute inset-x-0 h-px bg-gray-300"></span>
-            <span className="relative bg-white px-4 text-sm text-gray-400">Log in with social</span>
+            <span className="absolute inset-x-0 h-px bg-green-300"></span>
+            <span className="relative bg-white px-4 text-sm text-green-600">Log in with social</span>
           </div>
   
         
   
-          <button className="flex items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-gray-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base">
+          <button className="flex items-center justify-center gap-2 rounded-lg border border-green-300 bg-white px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-gray-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base">
             <svg className="h-5 w-5 shrink-0" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M23.7449 12.27C23.7449 11.48 23.6749 10.73 23.5549 10H12.2549V14.51H18.7249C18.4349 15.99 17.5849 17.24 16.3249 18.09V21.09H20.1849C22.4449 19 23.7449 15.92 23.7449 12.27Z" fill="#4285F4" />
               <path d="M12.2549 24C15.4949 24 18.2049 22.92 20.1849 21.09L16.3249 18.09C15.2449 18.81 13.8749 19.25 12.2549 19.25C9.12492 19.25 6.47492 17.14 5.52492 14.29H1.54492V17.38C3.51492 21.3 7.56492 24 12.2549 24Z" fill="#34A853" />
@@ -40,13 +42,14 @@ function Login() {
           </button>
         </div>
   
-        <div className="flex items-center justify-center bg-gray-100 p-4">
-         <p className="text-center text-sm text-gray-500">Don't have an account? <Link to="/signup" > <a href="#" className="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Register</a></Link></p>
+        <div className="flex items-center justify-center bg-green-100 p-4">
+         <p className="text-center text-sm text-gray-500">Don't have an account? <Link to="/signup" > <a href="#" className="text-green-500 transition duration-100 hover:text-green-600 active:text-green-700">Register</a></Link></p>
         </div>
       </form>
     </div>
   </div>
   </div>
+  </>
   )
 }
 
