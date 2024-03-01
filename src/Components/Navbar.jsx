9b9f60bbc0c0cd9c1404ea986ffa16d950886c93
Darshan Khapekar <darshan.khapekar@vpt.edu.in>
2024-03-01 19:45:02 +0530

    Login/Profile Changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
   const [hasEmail, sethasEmail] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleClickProfile = () => {
+    navigate("/profile");
+  };
 
   useEffect(() => {
     const storedEmail = localStorage.getItem("email");
@@ -23,22 +29,23 @@ function Navbar() {
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
-            <a className="flex font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
-                viewBox="0 0 24 24"
-                onClick={toggleMenu}
-              >
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
-              <span className="ml-3 text-xl">Waste Wise Web</span>
-            </a>
+            <Link to="/">
+              <a className="flex font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span className="ml-3 text-xl">Waste Wise Web</span>
+              </a>
+            </Link>
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
@@ -75,13 +82,23 @@ function Navbar() {
             </nav>
 
             <div className="flex items-center gap-4">
-              <div className="sm:flex sm:gap-4">
-                <Link to="/login" className="flex items-center">
-                  <a className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow">
-                    Login
-                  </a>
-                </Link>
-              </div>
+              {hasEmail ? (
+                <div className="sm:flex sm:gap-4">
+                  <Link to="/profile" className="flex items-center">
+                    <a className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow">
+                      Profile
+                    </a>
+                  </Link>
+                </div>
+              ) : (
+                <div className="sm:flex sm:gap-4">
+                  <Link to="/login" className="flex items-center">
+                    <a className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow">
+                      Login
+                    </a>
+                  </Link>
+                </div>
+              )}
 
               <div className="block md:hidden">
                 <button
