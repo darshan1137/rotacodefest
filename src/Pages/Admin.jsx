61245463f55d8e7c7bb347e02916d17833018c90
ShravaniR2412 <shravanirasam0212@gmail.com>
2024-03-06 00:13:16 +0530

    admin styling

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -145,33 +145,49 @@ export default function Blogs() {
         </p>
       </div>
 
-      <div className="m-5 grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
+
+      <div className="mx-auto max-w-lg text-center">
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              Blogs Approval
+            </h2>
+          </div>
+
+      <div className=" m-10 px-10  grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
+        
         {blogs.map((blog) => (
           <div
             key={blog.id}
             className="group relative flex h-48 flex-col overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-64 xl:h-96"
           >
             <Link
-              to={`/readblog/${blog.id}`}
+            key={blog.id}
+            to={`/readblog/${blog.id}`}
+            className="group relative flex h-48 flex-col overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-64 xl:h-96"
+          >
+            <img
+              src={blog.imglink}
+              loading="lazy"
+              alt={`Photo for ${blog.title}`}
               className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-            >
-              <img
-                src={blog.imglink}
-                loading="lazy"
-                alt={`Photo for ${blog.title}`}
-              />
-              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent md:via-transparent"></div>
-              <div className="relative mt-auto p-4">
-                <h2 className="mb-1 text-xl font-semibold text-white transition duration-100">
-                  {blog.title}
-                </h2>
-                <p className="text-gray-300 text-sm mb-2">{blog.subtitle}</p>
-                <span className="block text-sm text-gray-200">
-                  {new Date(blog.timestamp.seconds * 1000).toLocaleString()}
-                </span>
-                <span className="font-semibold text-indigo-300">Read more</span>
-              </div>
-            </Link>
+            />
+
+            <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent md:via-transparent"></div>
+
+            <div className="relative mt-auto p-4">
+              <h2 className="mb-1 text-xl font-semibold text-white transition duration-100">
+                {blog.title}
+              </h2>
+              <p className="text-gray-300 text-sm mb-2">{blog.subtitle}</p>{" "}
+              {/* Added subtitle here */}
+              <span className="block text-sm text-gray-200">
+                {new Date(blog.timestamp.seconds * 1000).toLocaleString()}
+              </span>
+              <span className="font-semibold text-indigo-300">Read more</span>
+            </div>
+          </Link>
+
+          <div className="m-8">
+
 
             {blog.status === "notapproved" && (
               <div className="absolute bottom-4 left-4 space-y-2">
@@ -189,6 +205,9 @@ export default function Blogs() {
                 </button>
               </div>
             )}
+            
+          </div>
+
           </div>
         ))}
       </div>
