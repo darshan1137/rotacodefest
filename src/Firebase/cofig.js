44d93b6a63291b02696995b84c28c294bc3f74b4
Darshan Khapekar <darshankhapekar8520@gmail.com>
2024-03-17 11:57:38 +0530

    Update cofig.js

--- src/Firebase/cofig.js
+++ src/Firebase/cofig.js
@@ -6,13 +6,7 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "ADD YOUR FIREBASE API KEY HERE",
-  authDomain: "codevista-b8ff8.firebaseapp.com",
-  projectId: "codevista-b8ff8",
-  storageBucket: "codevista-b8ff8.appspot.com",
-  messagingSenderId: "453770804559",Iza
-  appId: "1:453770804559:web:3b3cef4518a888dca8993a",
-  measurementId: "G-6D3CFQE3T4"
+  //Firebase Credentials
 };
 
 const app = initializeApp(firebaseConfig);
